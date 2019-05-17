7bd41e20bb50ad4f67081e8e8d2748571a0fa06e
Edward-1020 <iezzuyoung@gmail.com>
2019-05-17 18:02:14 +0800

    feat: flyv-doc init

---
+++ packages/flyv-doc/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flyv-doc">
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+    name: 'flyv-doc'
+}
+</script>
+
+<style scoped lang="scss" rel="stylesheet/scss">
+</style>
---
+++ packages/flyv-doc/src/pages/head/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <head class="flyv-doc-header">
+    </head>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+    name: 'flyv-doc-header'
+}
+</script>
+
+<style scoped lang="scss" rel="stylesheet/scss">
+@import '../../style/variable.scss';
+
+.flyv-doc-header {
+    width: 100%;
+    min-height: 60px;
+    border-bottom: 1px solid $flyv-doc-border-color;
+}
+</style>
