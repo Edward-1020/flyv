8df799c3f32eaaf82d7a7d43426614f6b97e344d
Edward-1020 <iezzuyoung@gmail.com>
2019-05-21 16:18:18 +0800

    feat: nav-link

--- packages/flyv-doc/src/pages/nav/index.vue
+++ packages/flyv-doc/src/pages/nav/index.vue
@@ -1,9 +1,34 @@
 <template>
+    <div class="flyv-doc-nav">
+        <div
+            v-for="(item, index) in config"
+            :key="index"
+            class="flyv-doc-nav__item">
+            <div class="flyv-doc-nav__title">{{item.name}}</div>
+            <div
+                v-for="(groupItem, index) in item.groups"
+                :key="index">
+                <div class="flyv-doc-nav__group-title">{{groupItem.name}}</div>
+                <flyv-doc-nav-link
+                    v-for="(navItem, index) in groupItem.list"
+                    :key="index"
+                    :config="navItem"/>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script type="text/ecmascript-6">
+import NavLink from './nav-link/index';
+
 export default {
-    name: 'flyv-doc-nav'
+    name: 'flyv-doc-nav',
+    props: {
+        config: Array
+    },
+    components: {
+        [NavLink.name]: NavLink
+    }
 }
 </script>
 
---
+++ packages/flyv-doc/src/pages/nav/nav-link/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <router-link :to="config.path" v-html="config.name"/>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+    name: 'flyv-doc-nav-link',
+    props: {
+        config: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss" rel="stylesheet/scss">
+</style>
