588adbaf9a0f5bbd25ecde374f2100c83018a678
Edward-1020 <iezzuyoung@gmail.com>
2019-06-11 19:33:48 +0800

    feat: 类型限制

--- packages/flyv-ui/docs/config/flyv-doc/index.ts
+++ packages/flyv-ui/docs/config/flyv-doc/index.ts
@@ -1,17 +1,19 @@
-export default {
-    nav: [
-      {
-        name: '基础组件',
-        groups: [
-          {
-            list: [
-              {
-                name: 'Progress 进度条',
-                path: '/#/progress'
-              }
-            ]
-          }
-        ]
-      }
-    ]
+const flyvDocConfig = {
+  nav: [
+    {
+      name: '基础组件',
+      groups: [
+        {
+          list: [
+            {
+              name: 'Progress 进度条',
+              path: '/#/progress'
+            }
+          ]
+        }
+      ]
+    }
+  ]
 }
+
+export default 
--- packages/flyv-ui/docs/config/md/index.ts
+++ packages/flyv-ui/docs/config/md/index.ts
@@ -1,7 +1,15 @@
-export default [
+interface route {
+    path: string,
+    name: string,
+    component: () => {}
+}
+
+const routes: route[] = [
     {
         path: '/progress',
         name: 'Progress',
         component: () => import('../../../packages/progress/index.md')
     }
 ];
+
+export default routes;
