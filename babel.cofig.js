a034ff6ea5e0f7b90cc3e82bd283aa5bd4cce6e2
Edward-1020 <iezzuyoung@gmail.com>
2019-07-10 13:48:45 +0800

    feat: babel不处理module

--- babel.cofig.js
+++ babel.cofig.js
@@ -1,6 +1,11 @@
 module.exports = function () {
     const presets = [
-        '@babel/preset-env',
+        [
+            '@babel/preset-env',
+            {
+                modules: false
+            }
+        ],
         '@babel/preset-typescript'
     ];
     const plugins = [
